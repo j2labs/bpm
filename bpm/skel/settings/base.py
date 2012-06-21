644c0ebfdf8c77396216659155d7b700e514de03
James Dennis <jd@j2labs.net>
2012-06-21 14:39:08 -0400

    Changed the name of the mongrel2 variables and adjusted the logs directory path

--- bpm/skel/settings/base.py
+++ bpm/skel/settings/base.py
@@ -16,7 +16,7 @@ dir_project = os.path.abspath(os.path.dirname(cwd))  ### cwd/../
 dir_bin = os.path.join(dir_project, 'bin/')
 dir_virtualenv = os.path.join(dir_project, '.virtualenv/')
 dir_settings = os.path.join(dir_project, 'settings/')
-dir_logs = os.path.join(dir_project, 'log/')
+dir_logs = os.path.join(dir_project, '.var/log/')
 dir_static = os.path.join(dir_project, 'static/')
 dir_templates = os.path.join(dir_project, 'templates/')
 
@@ -25,5 +25,5 @@ dir_templates = os.path.join(dir_project, 'templates/')
 ### Mongrel2
 ###
 
-m2_conf = os.path.join(dir_settings, 'm2.conf')
-m2_db = os.path.join(dir_settings, 'm2.db')
+mongrel2_conf = os.path.join(dir_settings, 'settings/mongrel2.conf')
+mongrel2_db = os.path.join(dir_settings, 'settings/mongrel2.db')
