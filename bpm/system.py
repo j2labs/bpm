f26852abffcecefabcdd5956ebe9844dc9593b10
James Dennis <jd@j2labs.net>
2012-06-23 14:00:45 -0400

    Expanded bpm.system by adding code to import a module directory path, or a module file, or to find the settings directory. Basically, anything system related will go here.

--- bpm/system.py
+++ bpm/system.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import tempfile
 import subprocess
@@ -19,4 +20,50 @@ def run_as_script(settings, script):
     cmd = ['sh', tempname]
     return subprocess.check_call(cmd)
 
+
+def walk_up_until(root_path, sub_path):
+    """Starts at `root_path` and basically loops through `cd ..` until it finds
+    a directory that contains `sub_path`, or it reaches root.
+
+    It returns the directory that had `sub_path` in it on success and returns
+    None if nothing is found.
+    """
+    cur_dir = root_path
+    while cur_dir != os.sep:
+        potential_path = os.path.join(cur_dir, sub_path)
+        if os.path.exists(potential_path):
+            return potential_path
+        cur_dir = os.path.dirname(cur_dir)
+    return None
+
+
+def import_dir(full_path_to_module):
+    ### Chop off trailing '/' if present
+    if full_path_to_module[-1] == os.sep:
+        full_path_to_module = full_path_to_module[0:-1]
+    module_dir, module_name = os.path.split(full_path_to_module)
+    sys.path.insert(0, module_dir)
+    module_obj = __import__(module_name)
+    module_obj.__file__ = full_path_to_module
+    sys.path.remove(module_dir)
+    return module_obj
+
+
+def import_file(full_path_to_module):
+    """I don't like doing this, but I don't know of a better way. If you are
+    reading this code and have a solution, please send one.
+
+    In the meantime, I must credit this Stack Overflow: http://bit.ly/PPf9y0
+    """
+    try:
+        module_dir, module_file = os.path.split(full_path_to_module)
+        module_name, module_ext = os.path.splitext(module_file)
+        save_cwd = os.getcwd()
+        os.chdir(module_dir)
+        module_obj = __import__(module_name)
+        module_obj.__file__ = full_path_to_module
+        globals()[module_name] = module_obj
+        os.chdir(save_cwd)
+    except:
+        raise ImportError
 
