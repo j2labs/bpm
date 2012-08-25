a70df16e9e7ea9b32c10af9f69dfc669a7b68b48
James Dennis <jd@j2labs.net>
2012-08-25 15:37:16 -0400

    API app to go along with web/api split.

---
+++ bpm/skel/project/api.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+
+from brubeck.request_handling import Brubeck
+from brubeck.connections import Mongrel2Connection
+
+from settings import init_db_conn, init_msg_conn, log_level, cookie_secret
+from {{BPM_PROJECT_NAME}}.handlers import APILandingHandler
+
+
+### Instantiate connections
+db_conn = init_db_conn()
+msg_conn = init_msg_conn()
+
+
+### Routes
+handler_tuples = [
+    (r'^/', APILandingHandler),
+]
+
+
+### App config
+app_config = {
+    'msg_conn': msg_conn,
+    'handler_tuples': handler_tuples,
+    'db_conn': db_conn,
+    'cookie_secret': cookie_secret,
+    'log_level': log_level,
+}
+
+
+### Instances
+app = Brubeck(**app_config)
+app.run()
